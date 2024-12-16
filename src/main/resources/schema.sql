c01234fc682582859186157a909aff2cfa5630d8
kyky <kyle.vuanh@gmail.com>
2024-12-16 21:12:36 +0700

    [UPDATE] Database Unique Name

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS Ninja (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(20) NOT NULL,        
+    name VARCHAR(20) UNIQUE NOT NULL,        
     skill1 VARCHAR(255) NOT NULL,                
     skill2 VARCHAR(255) NOT NULL,                
     skill3 VARCHAR(255) NOT NULL,                
