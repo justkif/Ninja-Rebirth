cf5a2653b6242b0a63580fd1942e515d521dc275
kyky <kyle.vuanh@gmail.com>
2024-12-16 19:03:55 +0700

    [CREATE] Project Initialization

---
+++ src/main/java/dev/kyky/NR/NrApplication.java
@@ -0,0 +1,13 @@
+package dev.kyky.NR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NrApplication.class, args);
+	}
+
+}
