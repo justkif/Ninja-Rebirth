14917331ce6aa69df6c90a9fa79c8fe448c1cf85
kyky <kyle.vuanh@gmail.com>
2024-12-19 21:21:32 +0700

    [CREATE] getAll && registerUser

--- src/main/java/dev/kyky/NR/Controllers/UserController.java
+++ src/main/java/dev/kyky/NR/Controllers/UserController.java
@@ -1,9 +1,19 @@
 package dev.kyky.NR.Controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.kyky.NR.Exceptions.UserConflictException;
+import dev.kyky.NR.Models.User;
 import dev.kyky.NR.Services.UserService;
+import jakarta.validation.Valid;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("/")
@@ -13,6 +23,18 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    List<User> getAll() {
+        return userService.getAll();
+    }    
+
+    @PostMapping("/register")
+    void registerUser(@Valid @RequestBody User user) {
+        if (!userService.registerUser(user)) {
+            throw new UserConflictException();
+        }
     }
 
 }
--- src/main/java/dev/kyky/NR/Services/UserService.java
+++ src/main/java/dev/kyky/NR/Services/UserService.java
@@ -1,16 +1,34 @@
 package dev.kyky.NR.Services;
 
+import java.util.List;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import dev.kyky.NR.Models.User;
 import dev.kyky.NR.Repositories.UserRepository;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public boolean registerUser(User user) {
+        if (!userRepository.findByExactName(user.username()).isEmpty()) {
+            return false;
+        }
+        userRepository.save(user.setHashedPassword(passwordEncoder.encode(user.password())));
+        return true;
     }
 
 }
