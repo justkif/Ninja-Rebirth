dc6be81da69b4d3ddfce3402a33641fe5a5ae2d0
kyky <kyle.vuanh@gmail.com>
2024-12-19 21:21:13 +0700

    [CREATE] findByExactName

--- src/main/java/dev/kyky/NR/Repositories/UserRepository.java
+++ src/main/java/dev/kyky/NR/Repositories/UserRepository.java
@@ -1,9 +1,15 @@
 package dev.kyky.NR.Repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
 import dev.kyky.NR.Models.User;
 
 public interface UserRepository extends ListCrudRepository<User, Integer> {
+
+    @Query("SELECT * FROM \"user\" WHERE username = :username")
+    Optional<User> findByExactName(String username);
 
 }
