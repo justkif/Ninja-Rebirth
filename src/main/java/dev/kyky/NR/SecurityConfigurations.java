5ef6607818bbf54911fd39f404b8da98a33a8781
kyky <kyle.vuanh@gmail.com>
2024-12-19 21:20:06 +0700

    [CREATE] Security Configurations

---
+++ src/main/java/dev/kyky/NR/SecurityConfigurations.java
@@ -0,0 +1,28 @@
+package dev.kyky.NR;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfigurations {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorizeHttpRequests -> 
+                authorizeHttpRequests
+                .anyRequest().permitAll()
+            )
+            .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
