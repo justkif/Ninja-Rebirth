e3544143042dd985ae07e4dbccd3af6cdbe51d95
kyky <kyle.vuanh@gmail.com>
2025-01-23 16:31:48 +0700

    Update GlobalExceptionHandler.java

--- src/main/java/dev/kyky/NR/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/dev/kyky/NR/Exceptions/GlobalExceptionHandler.java
@@ -1,14 +1,9 @@
 package dev.kyky.NR.Exceptions;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import jakarta.validation.ConstraintViolationException;
 
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
