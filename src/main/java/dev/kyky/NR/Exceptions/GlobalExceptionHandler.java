321bbdbbfc8686e3a26ad6fdbd3340f422309cfe
kyky <kyle.vuanh@gmail.com>
2025-02-22 18:49:24 +0700

    Next: Test API

--- src/main/java/dev/kyky/NR/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/dev/kyky/NR/Exceptions/GlobalExceptionHandler.java
@@ -1,39 +1,39 @@
-// package dev.kyky.NR.Exceptions;
+package dev.kyky.NR.Exceptions;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 
-// import org.springframework.web.bind.MethodArgumentNotValidException;
-// import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
-// @ControllerAdvice
-// public class GlobalExceptionHandler {
+@ControllerAdvice
+public class GlobalExceptionHandler {
 
-//     @ExceptionHandler(NinjaConflictException.class)
-//     public ResponseEntity<String> handleNinjaConflict(NinjaConflictException ex) {
-//         return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
-//     }
+    @ExceptionHandler(NinjaConflictException.class)
+    public ResponseEntity<String> handleNinjaConflict(NinjaConflictException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
 
-//     @ExceptionHandler(NinjaNotFoundException.class)
-//     public ResponseEntity<String> handleNinjaNotFound(NinjaNotFoundException ex) {
-//         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-//     }
+    @ExceptionHandler(NinjaNotFoundException.class)
+    public ResponseEntity<String> handleNinjaNotFound(NinjaNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
 
-//     @ExceptionHandler(UserConflictException.class)
-//     public ResponseEntity<String> handleUserConflict(UserConflictException ex) {
-//         return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
-//     }
+    @ExceptionHandler(UserConflictException.class)
+    public ResponseEntity<String> handleUserConflict(UserConflictException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
 
-//     @ExceptionHandler(UserUnauthorizedException.class)
-//     public ResponseEntity<String> handleUserUnauthorized(UserUnauthorizedException ex) {
-//         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
-//     }
+    @ExceptionHandler(UserUnauthorizedException.class)
+    public ResponseEntity<String> handleUserUnauthorized(UserUnauthorizedException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
 
-//     @ExceptionHandler(MethodArgumentNotValidException.class)
-//     public ResponseEntity<String> handleMethodArgumentNotValidException() {
-//         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad Request.");
-//     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad Request.");
+    }
 
-// }
+}
