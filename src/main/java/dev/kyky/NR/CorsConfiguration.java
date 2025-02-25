50d4cac37e34ba05f0fea5bb5bd7a72736b31f9f
kyky <kyle.vuanh@gmail.com>
2025-02-25 13:34:56 +0700

    Update CorsConfiguration.java

--- src/main/java/dev/kyky/NR/CorsConfiguration.java
+++ src/main/java/dev/kyky/NR/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://ninja-rebirth.onrender.com")
+                .allowedOrigins("https://http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Token")
                 .allowCredentials(true);
